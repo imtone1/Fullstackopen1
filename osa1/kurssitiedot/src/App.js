b7d3ec489be57b2b0b3a717e7f441818a954d83b
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-12-24 21:48:49 +0200

    osa1, kurssitiedot 1.1, 1.2

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,71 @@
+const Header = (props) => {
+  // console.log(props.course)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props) => {
+  // console.log(props.parts[0])
+  return (
+    <div>
+     
+        <Part part={props.parts[0]}/>
+        <Part part={props.parts[1]}/>
+        <Part part={props.parts[2]}/>
+
+    </div>
+  )
+}
+
+const Part = (props)=>{
+  // console.log(props.part)
+  return(
+    <>
+    <p>{props.part.part} {props.part.exercises}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  // console.log(props.parts[0].exercises + props.parts[1].exercises+props.parts[2].exercises)
+  return (
+    <div>
+     <p>
+        Total {props.parts[0].exercises + props.parts[1].exercises+props.parts[2].exercises}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+        {
+            part: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            part: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            part: 'State of a component',
+            exercises: 14
+        }
+    ]
+    
+}
+// console.log(course.parts[0].part)
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+     
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+export default App
