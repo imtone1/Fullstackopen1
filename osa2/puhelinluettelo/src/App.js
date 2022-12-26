825a29f415b1b8b73dd538ddcbb5652b970c5e14
imtone1 <88165529+imtone1@users.noreply.github.com>
2022-12-26 15:42:55 +0200

    osa2 puhelinluettelo 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonsForm from './components/PersonForm'
 import Person from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    // { name: 'Arto Hellas', number: '040-123456' },
+    // { name: 'Ada Lovelace', number: '39-44-5323523' },
+    // { name: 'Dan Abramov', number: '12-43-234345' },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const person = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
 
